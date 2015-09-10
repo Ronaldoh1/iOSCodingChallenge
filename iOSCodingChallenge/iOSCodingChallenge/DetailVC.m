5f8426b92644db709c4f8b57a09e925299b42c00
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-09-10 15:19:21 -0700

    able to download products and properly display them in a list

--- iOSCodingChallenge/iOSCodingChallenge/DetailVC.m
+++ iOSCodingChallenge/iOSCodingChallenge/DetailVC.m
@@ -11,6 +11,7 @@
 @interface DetailVC ()
 @property (weak, nonatomic) IBOutlet UITextView *descriptionTextView;
 @property (weak, nonatomic) IBOutlet UIImageView *productImage;
+@property (weak, nonatomic) IBOutlet UILabel *priceLabel;
 
 @end
 
@@ -20,15 +21,26 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
+
+    /*Populate Product information to provide more detail for user. */
+
     self.descriptionTextView.text = self.product.productDescription;
 
-
-   NSURL *imageURL = [NSURL URLWithString:self.product.imageURL];
-   NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
-
-
+    NSURL *imageURL = [NSURL URLWithString:self.product.imageURL];
+    NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
 
     self.productImage.image = [UIImage imageWithData:imageData];
+    self.priceLabel.text =[NSString stringWithFormat:@"$%@", self.product.price];
+
+    //Set the title for the VC.
+    UILabel *titleView = (UILabel *)self.navigationItem.titleView;
+    titleView = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 30)];
+    titleView.font = [UIFont fontWithName:@"Helvetica Bold" size:20];
+    titleView.text = self.product.productName;
+    titleView.textColor = [UIColor whiteColor];
+    [self.navigationItem setTitleView:titleView];
+    
+    
 }
 
 
